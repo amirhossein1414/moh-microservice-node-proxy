9c07f509c773a30b04ddaf83dc70059beff148ca
Rezaei, Hossein <h.rezaei@mohaymen.co>
2020-02-17 13:42:59 +0330

    adding-listeners

--- main.js
+++ main.js
@@ -1,17 +1,58 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+var http = require('http');
+var httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({
-    target: 'http://192.168.30.82:58181',
-    ws: true,
-    timeout:999999,
-    proxyTimeout:999999
-}).listen(58081); 
+let amsProxy = new httpProxy();
+let diasProxy = new httpProxy();
 
-httpProxy.createProxyServer({
-    target: 'http://192.168.30.84:58188',
-    ws: true,
-    timeout:999999,
-    proxyTimeout:999999
-}).listen(58088); 
+try {
+    createProxy();
+} catch (error) {
+    if (amsProxy != null)
+        amsProxy.close();
 
+    if (diasProxy != null)
+        diasProxy.close();
+
+    createProxy();
+}
+
+function createProxy() {
+    console.log('started');
+    let amsProxy = httpProxy.createProxyServer({
+        target: 'http://192.168.30.82:58181',
+        ws: true,
+        timeout: 999999,
+        proxyTimeout: 999999
+    }).listen(58081).on('proxyReq-58081', (proxyreq, req, res, options) => {
+        console.log('proxyReq-58081');
+    }).on('proxyReqWs-58081', (proxyreq, req, socket, options, head) => {
+        console.log('proxyReqWs-58081');
+    }).on('proxyRes', (proxyreq, req, res) => {
+        console.log('proxyRes-58081');
+    }).on('error', (err, req, res, target) => {
+        console.log('error-58081');
+    }).on('close', (proxyres, socket, proxyhead) => {
+        console.log('close-58081');
+    }).on('end', (req, res, proxyres) => {
+        console.log('end-58081');
+    });
+
+    diasProxy = httpProxy.createProxyServer({
+        target: 'http://192.168.30.84:58188',
+        ws: true,
+        timeout: 999999,
+        proxyTimeout: 999999
+    }).listen(58088).on('proxyReq-58088', (proxyreq, req, res, options) => {
+        console.log('proxyReq-58088');
+    }).on('proxyReqWs-58088', (proxyreq, req, socket, options, head) => {
+        console.log('proxyReqWs-58088');
+    }).on('proxyRes', (proxyreq, req, res) => {
+        console.log('proxyRes-58088');
+    }).on('error', (err, req, res, target) => {
+        console.log('error-58088');
+    }).on('close', (proxyres, socket, proxyhead) => {
+        console.log('close-58088');
+    }).on('end', (req, res, proxyres) => {
+        console.log('end-58088');
+    });;
+}
